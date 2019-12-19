a5608ca8f9dd533f90cdd22e61f62dd75aeb7f8b
Bruno Felipe Leal Delfino <bruno.delfino1995@gmail.com>
2019-12-19 14:53:37 -0300

    Add config to fix DataGrip

---
+++ roles/system/files/etc/profile.d/awt.sh
@@ -0,0 +1,2 @@
+export _JAVA_AWT_WM_NONREPARTENTING=1
+export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.crossplatformlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
